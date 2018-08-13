437e723238a75e6d4e7721d4d1729eb12f5332b3
MerkulovAA <merkylovarman@mail.ru>
2018-08-13 21:14:04 +0400

    insert testValidation in AbstractJpaUserServiceTest

--- src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractJpaUserServiceTest.java
@@ -2,8 +2,15 @@ package ru.javawebinar.topjava.service;
 
 
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.JpaUtil;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Collections;
+import java.util.Date;
 
 public abstract class AbstractJpaUserServiceTest extends AbstractUserServiceTest {
 
@@ -13,5 +20,14 @@ public abstract class AbstractJpaUserServiceTest extends AbstractUserServiceTest
     @Before
     public void cleanHibernateCache() throws Exception {
         jpaUtil.clear2ndLevelHibernateCache();
+    }
+
+    @Test
+    public void testValidation() throws Exception {
+        validateRootCause(() -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "  ", "password", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.ROLE_USER)), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 9, true, new Date(), Collections.emptySet())), ConstraintViolationException.class);
+        validateRootCause(() -> service.create(new User(null, "User", "mail@yandex.ru", "password", 10001, true, new Date(), Collections.emptySet())), ConstraintViolationException.class);
     }
 }
