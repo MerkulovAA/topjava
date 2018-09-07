b8dd937e2ad4562086c6850fc815aca14340a869
MerkulovAA <merkylovarman@mail.ru>
2018-09-07 11:29:14 +0400

    Apply 10_12_controller_advice_exception

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.web.meal;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -42,6 +41,7 @@ public class MealAjaxController extends AbstractMealController {
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public ResponseEntity<String> createOrUpdate(@Valid Meal meal, BindingResult result) {
         if (result.hasErrors()) {
+            // TODO change to exception handler
             return ValidationUtil.getErrorResponse(result);
         }
         if (meal.isNew()) {
